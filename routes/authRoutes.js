0022a98357e9849b84976205a3da808c48a1fc3c
Smartysidd <Sidd271299@gmail.com>
2025-04-10 10:17:52 +0530

    fix: replaced bcrypt with bcryptjs in authRoutes for compatibility

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const pool = require("../config/db"); // correct path to db.js
 require("dotenv").config(); // for using .env variables
